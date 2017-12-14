2bad88d824f651ed220ca810cb1764490fff5f49
Muhammad youssef <muhammadyoussef.27@gmail.com>
2017-12-14 05:39:21 +0200

    setup schedulers

---
+++ anabeesh/src/main/java/com/cs18/anabeesh/schedulers/ThreadSchedulers.kt
@@ -0,0 +1,15 @@
+package com.cs18.anabeesh.schedulers
+
+import io.reactivex.Scheduler
+
+/**
+ * This interface is implemented by different classes (i.e. [IOThreadSchedulers]) to define which ThreadSchedulers
+ * should be used for RxJava's [io.reactivex.Observable.observeOn] and [io.reactivex.Observable.subscribeOn]
+ */
+
+interface ThreadSchedulers {
+
+    fun observeOn(): Scheduler
+
+    fun subscribeOn(): Scheduler
+}
---
+++ anabeesh/src/main/java/com/cs18/anabeesh/schedulers/qualifier/MainThread.kt
@@ -0,0 +1,10 @@
+package com.cs18.anabeesh.schedulers.qualifier
+
+import javax.inject.Qualifier
+
+/**
+ * This qualifier is used for distinguishing between different [com.cs18.anabeesh.schedulers.ThreadSchedulers] for dependency injection.
+ */
+
+@Qualifier
+annotation class MainThread
